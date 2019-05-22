ab46d688ed32dec069115e863776757d3602931d
Gianluca Scopelliti <gianlu.1033@gmail.com>
2019-05-23 00:50:21 +0200

    Use processes instead of threads. Managed Ctrl+C interrupt

--- genetic.py
+++ genetic.py
@@ -1,21 +1,64 @@
 import serializer
+import signal
 import sys
 import random
+import os
 import threading
 from solution import Solution
-from multiprocessing.dummy import Pool as ThreadPool
 import parameters as par
+from multiprocessing import Manager, Pool
 
 #other things
 population = []
-best_solution = Solution()
-lock = threading.Lock()
-stop = False
+
+manager = Manager()
+shared_list = manager.list()
+file_name_out = ""
+
+#handlers for signal
+def handler_main(signum, frame):
+    global file_name_out
+    serializer.write_to_file(population, file_name_out)
+    print("{0} Exiting main process..".format(os.getpid()))
+    sys.exit(0)
+
+def handler_worker(signum, frame):
+    print("Stopping work..")
+    sys.exit(0)
+
+#functions used in multiprocessing
+#TODO
+def local_search(child):
+    global shared_list
+    signal.signal(signal.SIGINT, handler_worker)
+
+    child.objf = random.randint(0, 5000)
+
+    shared_list.append(child)
+
+def compute_objf(array):
+    global shared_list
+    signal.signal(signal.SIGINT, handler_worker)
+
+    objf = -1
+
+    #start_analysys..
+    #TODO
+    objf = random.randint(0,5000)
+
+    sol = Solution(array, objf)
+
+    shared_list.append(sol)
+
+pool = Pool()
 
 def start(output_file, input_file = ""):
-    threads = []
-    global stop
+    global file_name_out
+    file_name_out = output_file
+    signal.signal(signal.SIGINT, handler_main)
     global population
+    global pool
+    global shared_list
 
     #setting the starting population
     if len(input_file) != 0:
@@ -26,57 +69,57 @@ def start(output_file, input_file = ""):
     #if there are less solutions than POPULATION_SIZE, add them
     initialize_solutions()
 
-    try:
-        while True:
-            print("Generation started.")
+    print_solutions()
 
-            #generate children
-            children = combination()
-
-            #local search in multithreading
-            for i in range(par.POPULATION_SIZE):
-                thread = threading.Thread(target=local_search, args=(children[i],))
-                threads.append(thread)
-                thread.start()
-
-            for thread in threads:
-                thread.join()
-            del threads[:]
-
-            #new population
-            del population[:]
-            population = children.copy()
-            del children[:]
-
-            print("Generation ended.")
+    population.sort(key=lambda solution: solution.objf, reverse=True)
 
 
-    except KeyboardInterrupt:
-        stop = True
-        serializer.write_to_file(population, output_file)
-        sys.exit(0)
+    while True:
+        print("Generation started.")
+
+        #generate children
+        children = combination()
+
+        pool.map(local_search, children)
+
+        del children[:]
+        while len(shared_list) > 0:
+            children.append(shared_list.pop(0))
+
+        #new population
+        del population[:]
+        population = children.copy()
+        del children[:]
+
+        population.sort(key=lambda solution: solution.objf, reverse=True)
+        print_solutions()
+
+        print("Generation ended.")
 
 
 def initialize_solutions():
     global population
-    threads = []
+    global pool
+    global shared_list
 
     numsol = par.POPULATION_SIZE - len(population)
 
-    for i in range(numsol):
-        thread = threading.Thread(target=new_random_solution)
-        threads.append(thread)
-        thread.start()
+    if numsol == 0:
+        return
 
-    for thread in threads:
-        thread.join()
-    del threads[:]
+    new_sols = []
+
+    for i in range(numsol):
+        new_sols.append(new_random_solution())
+
+    pool.map(compute_objf, new_sols)
+
+    while len(shared_list) > 0:
+        population.append(shared_list.pop(0))
+
 
 def new_random_solution():
-    global lock
-    global population
     array = []
-    value = -1
 
     probability = round(par.POPULATION_SIZE / par.AVERAGE_NUM_COLUMNS)
 
@@ -86,20 +129,14 @@ def new_random_solution():
         else:
             array.append(False)
 
-    #start simulation in order to obtain the value..
-    #TODO
-    value = random.randint(0,5000)
+    return array
 
-    sol = Solution(array, value)
-
-    with lock:
-        population.append(sol)
 
 
 def print_solutions():
     global population
 
-    print("Number of elements: {0}".format(len(population)))
+    #print("Number of elements: {0}".format(len(population)))
 
     for solution in population:
         print(solution.array)
@@ -111,13 +148,5 @@ def combination():
     return population.copy()
 
 #TODO
-def local_search(child):
-    global stop
-    global lock
-
-    while not stop:
-        print(threading.currentThread().getName())
-
-#TODO
 def check_best_solution():
-    global lock
+    pass
