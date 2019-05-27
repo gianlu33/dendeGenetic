61290113561572e251e72a3ac278d5622842fc3b
Gianluca Scopelliti <s256647@studenti.polito.it>
2019-05-27 13:22:50 +0200

    Remove old python code

--- old_python/parameters.py
+++
@@ -1,3 +0,0 @@
-#parameters for the algorithm
-POPULATION_SIZE = 20
-AVERAGE_NUM_COLUMNS = 8 #for initializing a new solution, we select this number of cells to be True
--- old_python/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    array = []
-    objf = -1
-
-    def __init__(self, array=[], objf=-1):
-        self.array = array
-        self.objf = objf
